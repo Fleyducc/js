a5629163f14c501f063c7172fd36635500823218
Fleyducc <silau893@gmail.com>
2024-05-26 20:05:29 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let ism = prompt("Ismingizni kiriting");
+console.log(ism);
+
+let familiya = prompt("Familiyangiz kiriting");
+console.log(familiya);
+
+let yosh = prompt("Yoshingizni kiriting");
+console.log(yosh);
+
+let fan = prompt("Yoqtirgan faningizni yozing");
+console.log(fan);
+
+let joy = prompt("Yashaydigan tumaningizni kiriting");
+console.log(joy);
+
+alert("malumotlaringizni kiritganingiz uchun tashakkur");
